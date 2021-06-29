c93154edf15bc7eb0480134523db3dc08fbb23cc
SatyaChachaundiya <53097134+SatyaChachaundiya@users.noreply.github.com>
2021-06-29 08:56:33 +0530

    dep. injection conti.

--- ng-fundamentals/src/app/user/profile.component.ts
+++ ng-fundamentals/src/app/user/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Inject } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
 import { AuthService } from './auth.service'
+import { TOASTR_TOKEN, Toastr } from '../common/toastr.service'
 
 @Component({
   templateUrl:'./profile.component.html',
@@ -22,7 +23,10 @@ export class ProfileComponent implements OnInit{
   private firstName:FormControl
   private lastName:FormControl
 
-  constructor(private authser:AuthService, private route:Router){
+  constructor(
+    private authser:AuthService, 
+    private route:Router,
+    @Inject(TOASTR_TOKEN) private toastr: Toastr){
 
   }
 
@@ -61,7 +65,8 @@ export class ProfileComponent implements OnInit{
     if(this.profileForm.valid){
       // console.log(pform);
       this.authser.updateCurrentuser(pform.firstName, pform.lastName)
-      this.route.navigate(['events'])
+      // this.route.navigate(['events'])
+      this.toastr.success('Profile Saved')
     }
   }
 
